af39e9fc13e758cad40d46b1e1ebcc20a3c6ce71
minnie <124010199+minheeV@users.noreply.github.com>
2023-07-28 21:20:18 +0900

    영화 검색바 그리기

--- movie_reviews/lib/main.dart
+++ movie_reviews/lib/main.dart
@@ -11,56 +11,72 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: false,
-          title: Text(
-            "Movie Reivews",
-            style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-          ),
-          backgroundColor: Color(0xFFFFFFFF),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.person_2_outlined),
-            ),
-          ],
+      home: MovieReviews(),
+    );
+  }
+}
+
+class MovieReviews extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text(
+          "Movie Reivews",
+          style: TextStyle(
+              fontSize: 28, fontWeight: FontWeight.bold, color: Colors.black),
         ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              children: [
-                //Padding(
-                //  padding: const EdgeInsets.all(32),
-                //  child: Image.network(
-                //    "https://i.ibb.co/nngK6j3/startup.png",
-                //    width: 81,
-                //  ),
-                //),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: '이메일',
-                  ),
-                ),
-                TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: '비밀번호',
-                  ),
-                ),
-                Container(
-                  width: double.infinity,
-                  margin: EdgeInsets.only(top: 24),
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Text("로그인"),
-                  ),
-                ),
-              ],
+        backgroundColor: Colors.white,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.person_2_outlined),
+            color: Colors.black,
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey, width: 1.0)),
+                  suffixIcon: Icon(Icons.search),
+                  hintText: '영화 제목을 검색해주세요.'),
             ),
           ),
-        ),
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  /*TextField(
+                      decoration: InputDecoration(
+                        labelText: '이메일',
+                      ),
+                    ),
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: '비밀번호',
+                      ),
+                    ),
+                    Container(
+                      width: double.infinity,
+                      margin: EdgeInsets.only(top: 24),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text("로그인"),
+                      ),
+                    ),*/
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
