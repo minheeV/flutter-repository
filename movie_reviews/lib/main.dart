fddd68d60da37d9403f44cb9f16b4677bba931ad
minnie <124010199+minheeV@users.noreply.github.com>
2023-07-30 14:15:21 +0900

    리스트 생성

--- movie_reviews/lib/main.dart
+++ movie_reviews/lib/main.dart
@@ -17,8 +17,38 @@ class MyApp extends StatelessWidget {
 }
 
 class MovieReviews extends StatelessWidget {
+  const MovieReviews({super.key});
+
   @override
   Widget build(BuildContext context) {
+    //데이터
+    List<Map<String, dynamic>> dataList = [
+      {
+        "category": "탑건: 매버릭",
+        "imgUrl": "https://i.ibb.co/sR32PN3/topgun.jpg",
+      },
+      {
+        "category": "마녀2",
+        "imgUrl": "https://i.ibb.co/CKMrv91/The-Witch.jpg",
+      },
+      {
+        "category": "범죄도시2",
+        "imgUrl": "https://i.ibb.co/2czdVdm/The-Outlaws.jpg",
+      },
+      {
+        "category": "헤어질 결심",
+        "imgUrl": "https://i.ibb.co/gM394CV/Decision-to-Leave.jpg",
+      },
+      {
+        "category": "브로커",
+        "imgUrl": "https://i.ibb.co/MSy1XNB/broker.jpg",
+      },
+      {
+        "category": "문폴",
+        "imgUrl": "https://i.ibb.co/4JYHHtc/Moonfall.jpg",
+      },
+    ];
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: false,
@@ -28,6 +58,7 @@ class MovieReviews extends StatelessWidget {
               fontSize: 28, fontWeight: FontWeight.bold, color: Colors.black),
         ),
         backgroundColor: Colors.white,
+        elevation: 0, // 입체감
         actions: [
           IconButton(
             onPressed: () {},
@@ -39,43 +70,51 @@ class MovieReviews extends StatelessWidget {
       body: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.all(16.0),
+            padding: const EdgeInsets.all(8.0),
             child: TextField(
               decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey, width: 1.0)),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
                   suffixIcon: Icon(Icons.search),
                   hintText: '영화 제목을 검색해주세요.'),
             ),
           ),
-          SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                children: [
-                  /*TextField(
-                      decoration: InputDecoration(
-                        labelText: '이메일',
-                      ),
-                    ),
-                    TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        labelText: '비밀번호',
-                      ),
-                    ),
-                    Container(
-                      width: double.infinity,
-                      margin: EdgeInsets.only(top: 24),
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        child: Text("로그인"),
-                      ),
-                    ),*/
-                ],
-              ),
-            ),
+          Divider(
+            height: 1,
           ),
+          Expanded(
+              child: ListView.builder(
+                  itemCount: dataList.length,
+                  itemBuilder: (context, index) {
+                    String category = dataList[index]['category'];
+                    String imgUrl = dataList[index]['imgUrl'];
+
+                    return Card(
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Image.network(
+                            imgUrl,
+                            width: double.infinity,
+                            height: 200,
+                            fit: BoxFit.cover,
+                          ),
+                          Container(
+                              width: double.infinity,
+                              height: 200,
+                              color: Colors.black.withOpacity(0.5)),
+                          Text(
+                            category,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 36,
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  }))
         ],
       ),
     );
